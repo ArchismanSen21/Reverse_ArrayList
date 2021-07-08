850e56acbb1c15850cb64d85e408caa8a7de6106
Archisman Sen <85049006+ArchismanSen21@users.noreply.github.com>
2021-07-08 15:02:50 +0530

    Update reverse_array.java

--- reverse_array.java
+++ reverse_array.java
@@ -1,27 +1,41 @@
-import java.util.Scanner;
-public class reverse_array
+package reverse_list;
+import java.util.*;
+class reverse
+{
+    public ArrayList<Integer> revarrlis (ArrayList<Integer> ar)
+    {
+        int m=ar.size();
+        for(int i=0; i<m/2;i++)
+        {
+            Integer t = ar.get(i);
+            ar.set(i,ar.get(m-1-i));
+            ar.set(m-1-i,t);
+        }
+        return ar;
+    }
+    public void printele(ArrayList<Integer> ar)
+    {
+        for (int i=0;i<ar.size();i++)
+            System.out.print(ar.get(i)+" ");
+    }
+}
+
+public class reve
 {
     public static void main(String args[])
     {
+        reverse obj = new reverse();
         Scanner sc = new Scanner(System.in);
-        int i,m,t;
-        System.out.println("Enter the elements of array : ");
-        m = sc.nextInt();
-        int[] n = new int [m];
-        System.out.println("Enter the array : ");
-        for (i=0;i<m;i++)
-            n[i]=sc.nextInt();
-        System.out.println("The original array is : ");
-        for(i=0;i<m;i++)
-            System.out.print("\t"+n[i]);
-        for (i=0;i<m/2;i++)
-        {
-            t=n[i];
-            n[i]=n[m-1-i];
-            n[m-1-i]=t;
-        }
-        System.out.println("\nThe reverse array is : ");
-        for(i=0;i<m;i++)
-            System.out.print("\t"+n[i]);
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        System.out.println("Enter the number of elements in list: ");
+        int n = sc.nextInt();
+        System.out.println("Appending the list : ");
+        for (int i=0;i<n;i++)
+            arr.add(sc.nextInt());
+        System.out.println("The List before reverse : ");
+        obj.printele(arr);
+        arr = obj.revarrlis(arr);
+        System.out.println("The List after reverse : ");
+        obj.printele(arr);
     }
 }
